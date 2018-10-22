8cf4a09643f9d31a86569999eb2f451ac1ac2b76
Cristian Ramirez <cristiano.rosetti@gmail.com>
2018-10-21 17:18:00 -0700

    adding subnets sg and files to provision lb

--- load_balancer.tf
+++ load_balancer.tf
@@ -1,21 +1,52 @@
-resource "aws_network_interface" "lb_network_interface" {
-  subnet_id = "${aws_subnet.subnet_lb.id}"
-  private_ips = ["10.10.4.1"]
-  tags {
-    Name = "terraform-demo"
-  }
-}
-
 resource "aws_instance" "lb" {
   ami           = "${var.aws_ami}"
   instance_type = "${var.instance_type}"
-
-  network_interface {
-    network_interface_id = "${aws_network_interface.lb_network_interface.id}"
-    device_index = 0
-  }
-
+  subnet_id = "${aws_subnet.subnet_lb.id}"
+  vpc_security_group_ids = ["${aws_security_group.sgweb.id}"]
+  associate_public_ip_address = true
+  
   tags {
     Name = "terraform-demo"
+  }
+
+  # Copies the haproxy.cfg file to /home/haproxy.cfg
+  provisioner "file" {
+    source      = "scripts/haproxy.cfg"
+    destination = "/home/haproxy.cfg"
+  }
+
+  provisioner "file" {
+    source      = "install-consul.sh"
+    destination = "/tmp/install-consul.sh"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /tmp/install-consul.sh",
+      "sh /tmp/install-consul.sh",
+    ]
+  }
+  
+  provisioner "file" {
+    source      = "install-docker.sh"
+    destination = "/tmp/install-docker.sh"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /tmp/install-docker.sh",
+      "sh /tmp/install-docker.sh",
+    ]
+  }
+  
+  provisioner "remote-exec" {
+    inline = [
+      "docker run -d \
+           --name haproxy \
+           -p 80:80 \
+           --restart unless-stopped \
+           -v /home/haproxy.cfg:/usr/local/etc/haproxy/haproxy.cfg:ro \
+           haproxy:1.6.5-alpine"
+    ]
   }
 }
--- main.tf
+++ main.tf
@@ -18,6 +18,8 @@ terraform {
 
 resource "aws_vpc" "my_vpc" {
   cidr_block = "10.0.0.0/16"
+  enable_dns_hostnames = true
+
   tags {
     Name = "terraform-demo"
   }
@@ -25,36 +27,103 @@ resource "aws_vpc" "my_vpc" {
 
 resource "aws_subnet" "subnet_a" {
   vpc_id = "${aws_vpc.my_vpc.id}"
-  cidr_block = "10.10.1.0/24"
+  cidr_block = "10.0.1.0/24"
   availability_zone = "${var.region}a"
   tags {
-    Name = "tf-example"
+    Name = "terraform-demo"
   }
 }
 
 resource "aws_subnet" "subnet_b" {
   vpc_id = "${aws_vpc.my_vpc.id}"
-  cidr_block = "10.10.2.0/24"
+  cidr_block = "10.0.2.0/24"
   availability_zone = "${var.region}b"
   tags {
-    Name = "tf-example"
+    Name = "terraform-demo"
   }
 }
 
 resource "aws_subnet" "subnet_c" {
   vpc_id = "${aws_vpc.my_vpc.id}"
-  cidr_block = "10.10.3.0/24"
+  cidr_block = "10.0.3.0/24"
   availability_zone = "${var.region}c"
   tags {
-    Name = "tf-example"
+    Name = "terraform-demo"
   }
 }
 
 resource "aws_subnet" "subnet_lb" {
   vpc_id = "${aws_vpc.my_vpc.id}"
-  cidr_block = "10.10.4.0/24"
+  cidr_block = "10.0.4.0/24"
   availability_zone = "${var.region}"
   tags {
-    Name = "tf-example"
+    Name = "terraform-demo"
+  }
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = "${aws_vpc.my_vpc.id}"
+
+  tags {
+    Name = "terraform-demo"
+  }
+}
+
+resource "aws_route_table" "web-public-rt" {
+  vpc_id = "${aws_vpc.my_vpc.id}"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.gw.id}"
+  }
+
+  tags {
+    Name = "terraform-demo"
+  }
+}
+
+# Assign the route table to the public Subnet
+resource "aws_route_table_association" "web-public-rt" {
+  subnet_id = "${aws_subnet.subnet_lb.id}"
+  route_table_id = "${aws_route_table.web-public-rt.id}"
+}
+
+# Define the security group for public subnet
+resource "aws_security_group" "sgweb" {
+  name = "terraform_demo_test_web"
+  description = "Allow incoming HTTP connections & SSH access"
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = -1
+    to_port = -1
+    protocol = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks =  ["0.0.0.0/0"]
+  }
+
+  vpc_id="${aws_vpc.my_vpc.id}"
+
+  tags {
+    Name = "terraform-demo"
   }
 }
