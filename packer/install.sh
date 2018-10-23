c18cb5a0f8982e02fc0072bd8121c83d6f8a4c69
Jairo Lopez <jaloare@gmail.com>
2018-10-23 14:30:43 -0700

    basic ami

--- packer/install.sh
+++ packer/install.sh
@@ -10,4 +10,4 @@ sudo mv /tmp/ansible /ansible
 
 echo "localhost ansible_connection=local" | sudo tee /etc/ansible/hosts
 
-sudo ansible-playbook /ansible/playbook.yml --tags install
+sudo ansible-playbook /ansible/playbook.yml
