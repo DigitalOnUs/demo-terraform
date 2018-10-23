f5300f50df29ce74f038f6e3aba23243a6d04267
Jairo Lopez <jaloare@gmail.com>
2018-10-22 20:24:03 -0700

    adding packer

---
+++ packer/install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -eux
+
+sudo apt-get install software-properties-common
+sudo add-apt-repository -y ppa:ansible/ansible
+sudo apt-get update
+sudo apt-get install -y ansible
+
+sudo mv /tmp/ansible /ansible
+
+echo "localhost ansible_connection=local" | sudo tee /etc/ansible/hosts
+
+sudo ansible-playbook /ansible/playbook.yml --tags install
