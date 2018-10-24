f28e1946441b327fcc67978a42baff42b79ad024
Cristian Ramirez <cristiano.rosetti@gmail.com>
2018-10-23 18:10:53 -0700

    adding files

--- scripts/run-docker-web.sh
+++ scripts/run-docker-web.sh
@@ -2,7 +2,7 @@
 
 # Create an html page with the ip of this node
 ip=$(ifconfig eth1 | grep 'inet addr' | awk '{ print substr($2,6) }')
-echo "<h1>$ip $(hostname)</h1>" > /home/vagrant/ip.html
+echo "<h1>$ip $(hostname)</h1>" > /ip.html
 
 # Run nginx via docker
 # mount the ip.html page as a volume in the root of the default nginx site
@@ -11,5 +11,5 @@ docker run -d \
            --name web \
            -p 8080:80 \
            --restart unless-stopped \
-           -v /home/vagrant/ip.html:/usr/share/nginx/html/ip.html:ro \
+           -v /ip.html:/usr/share/nginx/html/ip.html:ro \
            nginx
