06631e6d2d7aed2f2dd4e03ca2197ef6df3a85b3
Jairo Lopez <jaloare@gmail.com>
2018-10-23 18:50:50 -0700

    mv docker files

--- packer/roles/docker/files/docker-lb.sh
+++ packer/roles/docker/files/docker-lb.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-sudo cp /tmp/haproxy.cfg  .
 
 # Run haproxy in a docker container
 # Mount the haproxy config file
@@ -7,6 +6,5 @@ sudo docker run -d \
            --name haproxy \
            -p 80:80 \
            --restart unless-stopped \
-           -v haproxy.cfg:/usr/local/etc/haproxy/haproxy.cfg:ro \
+           -v /tmp/haproxy.cfg:/usr/local/etc/haproxy/haproxy.cfg:ro \
            haproxy:1.6.5-alpine
-
