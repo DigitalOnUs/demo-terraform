eba9c1d0ebe039a5bbaa1da1b82bb946af2950a3
Cristian Ramirez <cristiano.rosetti@gmail.com>
2018-10-23 21:36:01 -0700

    0.1.1 ami

--- variables.tf
+++ variables.tf
@@ -3,11 +3,7 @@ variable "secret_key" {}
 
 variable "aws_ami" {
   description = "AMI to be used for aws instances"
-<<<<<<< HEAD
-  default     = "ami-0a6eb6b51bb98545d"
-=======
   default     = "ami-07312e5f6a79452c8"
->>>>>>> ea1a39c5516c6b5b64a9ea750db88ebce89af67a
 }
 
 variable "region" {
